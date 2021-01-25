63cab4612610249a7d608d5d5c86a10cf7e2d151
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-25 23:25:54 +0200

    contact page

--- app/src/main/java/com/example/travel_journal/ContactActivity.java
+++ app/src/main/java/com/example/travel_journal/ContactActivity.java
@@ -2,13 +2,22 @@ package com.example.travel_journal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+
     }
 }
--- app/src/main/java/com/example/travel_journal/fragments/ContactFragment.java
+++ app/src/main/java/com/example/travel_journal/fragments/ContactFragment.java
@@ -1,5 +1,7 @@
 package com.example.travel_journal.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,13 +10,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.travel_journal.R;
 
 public class ContactFragment extends Fragment {
 
+
+    ImageButton github;
+    ImageButton linkedin;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        github = view.findViewById(R.id.contact_github);
+        linkedin = view.findViewById(R.id.contact_linkedin);
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/vladescualexandra");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.linkedin.com/in/alexandra-bianca-vlădescu-631999180/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
