1425a7f15ae9502c21a5aa6101385017aa1349c0
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-25 23:40:25 +0200

    about page

--- app/src/main/java/com/example/travel_journal/fragments/AboutFragment.java
+++ app/src/main/java/com/example/travel_journal/fragments/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.example.travel_journal.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,12 +10,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.travel_journal.R;
 
 public class AboutFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView credits = view.findViewById(R.id.about_credits_link);
+
+        credits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.flaticon.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
